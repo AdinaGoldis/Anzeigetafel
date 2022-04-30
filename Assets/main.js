0c7713fa5d59ad1e2cad9f75f61697dd748bcebb
“AdinaGoldis” <agoldis29@gmail.com>
2022-04-30 14:23:15 +0200

    Zwischenschritt

---
+++ Assets/main.js
@@ -0,0 +1,14 @@
+console.log("Test");
+
+let Homeplus1 = 0;
+let Homeplus2 = 0;
+let Homeplus3 = 0;
+
+function counter1() {
+    console.log(Homeplus1);
+    Homeplus1++;
+    console.log(Homeplus2);
+    Homeplus2 += 2;
+    console.log(Homeplus2);
+    Homeplus3 += 3;
+};
